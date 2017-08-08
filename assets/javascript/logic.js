341d6e7dc6c79f30f0a3859beaf2d0703191daf3
clintWilder <clintwilder1@gmail.com>
2017-08-08 08:49:48 -0600

    working files

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,72 +12,114 @@ firebase.initializeApp(config);
 
 
 
+var database = firebase.database();
 
-//time now
-var timeNow = moment();
-
-//reference to Firebase
-var dataRef = Firebase.database();
-
-//function to calculate minutes away
-function trainTime(trainFirst, trainFrequency) {
-    var trainFirst = moment(trainFirst, 'HH:mm');
-    var trainFrequency = parseInt(trainFrequency);
-    //var timeNowMinutes = (timeNow.hour() * 60) + timeNow.minute();
-    var trainFirstDate = moment(trainFirst, 'H mm');
-    //var trainFirstMinutes = (trainFirstDate.hour() * 60) + trainFirstDate.minute();
-    var a = moment().diff(moment.utc(trainFirstDate), "minutes");
-    var b = a % trainFrequency;
-    var c = trainFrequency - b;
-    return c;
-
-}
-
-
-//on submit
 $('#trainSubmit').on('click', function() {
+    var trainName = $('#trainName').val().trim();
+    var destination = $('#trainDestination').val().trim();
+    var firstTrain = moment($('#trainFirst').val().trim(), 'HH:mm').subtract(10, 'years').format('X');
+    var frequency = $('#trainFrequency').val().trim();
 
-    //get values
-    trainName = $('#trainName').val().trim();
-    trainDestination = $('#trainDestination').val().trim();
-    trainFrequency = $('#trainFrequency').val();
-    trainFirst = $('#trainFirst').val();
+    var newTrain = {
+        name: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+    }
 
-    //push data to Firebase	
-    dataRef.push({
-        trainName: trainName,
-        trainDestination: trainDestination,
-        trainFrequency: trainFrequency,
-        trainFirst: trainFirst,
-    })
+    database.ref().push(newTrain);
 
-    //clear values from form
-    $('#trainName').val('');
-    $('#trainDestination').val('');
-    $('#trainFrequency').val('');
-    $('#trainFirst').val('');
+    alert("Train Added");
 
-    // prevent page reload
+    $('trainName').val("");
+    $('trainDestination').val("");
+    $('trainFirst').val("");
+    $('trainFrequency').val("");
+
     return false;
 })
 
+database.ref().on('child_added', function(snapshot) {
+    var name = snapshot.val().name;
+    var destination = snapshot.val().destination;
+    var frequency = snapshot.val().frequency;
+    var firstTrain = snapshot.val().firstTrain;
 
-//get data from Firebase
-dataRef.on('child_added', function(childSnapshot, prevChildKey) {
-    //get minutes away from function	
-    var mAway = trainTime(childSnapshot.val().trainFirst, childSnapshot.val().trainFrequency);
-    //append info to rows
+    var remainder = moment().diff(moment.unix(firstTrain), 'minutes') % frequency;
+    var minutes = frequency - remainder;
+    var arrival = moment().add(minutes, 'm').format('hh:mm A');
 
-    var row = $('<tr>');
-    row.append('<td class="trainName digital">' + childSnapshot.val().trainName + '</td>');
-    row.append('<td class="trainDestination digital">' + childSnapshot.val().trainDestination + '</td>');
-    row.append('<td class="trainFrequency digital">' + childSnapshot.val().trainFrequency + '</td>');
-    row.append('<td class="nextArrival digital">' + moment().add(mAway, 'minute').format('hh:mm') + '</td>');
-    row.append('<td class="minutesAway digital">' + mAway + '</td>');
-    $('#scheduleTable').append(row);
+    $('#scheduleTable').append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + arrival + "</td><td>" + minutes + "</td></tr>");
+})
 
-}, function(errorObject) {
 
-    console.log("The read failed: " + errorObject.code);
 
-});
+
+// //time now
+// var timeNow = moment();
+
+// //reference to Firebase
+// var dataRef = Firebase.database();
+
+// //function to calculate minutes away
+// function trainTime(trainFirst, trainFrequency) {
+//     var trainFirst = moment(trainFirst, 'HH:mm');
+//     var trainFrequency = parseInt(trainFrequency);
+//     //var timeNowMinutes = (timeNow.hour() * 60) + timeNow.minute();
+//     var trainFirstDate = moment(trainFirst, 'H mm');
+//     //var trainFirstMinutes = (trainFirstDate.hour() * 60) + trainFirstDate.minute();
+//     var a = moment().diff(moment.utc(trainFirstDate), "minutes");
+//     var b = a % trainFrequency;
+//     var c = trainFrequency - b;
+//     return c;
+
+// }
+
+
+// //on submit
+// $('#trainSubmit').on('click', function() {
+
+//     //get values
+//     trainName = $('#trainName').val().trim();
+//     trainDestination = $('#trainDestination').val().trim();
+//     trainFrequency = $('#trainFrequency').val();
+//     trainFirst = $('#trainFirst').val();
+
+//     //push data to Firebase	
+//     dataRef.push({
+//         trainName: trainName,
+//         trainDestination: trainDestination,
+//         trainFrequency: trainFrequency,
+//         trainFirst: trainFirst,
+//     })
+
+//     //clear values from form
+//     $('#trainName').val('');
+//     $('#trainDestination').val('');
+//     $('#trainFrequency').val('');
+//     $('#trainFirst').val('');
+
+//     // prevent page reload
+//     return false;
+// })
+
+
+// //get data from Firebase
+// dataRef.on('child_added', function(childSnapshot, prevChildKey) {
+//     //get minutes away from function	
+//     var mAway = trainTime(childSnapshot.val().trainFirst, childSnapshot.val().trainFrequency);
+//     //append info to rows
+
+//     var row = $('<tr>');
+//     row.append('<td class="trainName digital">' + childSnapshot.val().trainName + '</td>');
+//     row.append('<td class="trainDestination digital">' + childSnapshot.val().trainDestination + '</td>');
+//     row.append('<td class="trainFrequency digital">' + childSnapshot.val().trainFrequency + '</td>');
+//     row.append('<td class="nextArrival digital">' + moment().add(mAway, 'minute').format('hh:mm') + '</td>');
+//     row.append('<td class="minutesAway digital">' + mAway + '</td>');
+//     $('#scheduleTable').append(row);
+
+// }, function(errorObject) {
+
+//     console.log("The read failed: " + errorObject.code);
+
+// });
